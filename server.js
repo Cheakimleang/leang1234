d2c51ac0a7923dba036a9cb622a0b35ecc987632
Cheakimleang <67309872+Cheakimleang@users.noreply.github.com>
2020-07-15 21:19:03 +0700

    w
    w

---
+++ server.js
@@ -0,0 +1,70 @@
+const discord = require("discord.js")
+const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { PREFIX } = require("./config.json")
+
+//CLIENT EVENTS
+client.on('ready', () => {
+  function randomStatus() {
+    let status = ["Srolanh ke ta eng", "-lplay", "Bot"]
+    let rstatus = Math.floor(Math.random() * status.length);
+
+    client.user.setActivity(status[rstatus], {type: "WATCHING"});
+  }; setInterval(randomStatus, 10000)
+  client.user.setStatus('Do Not Disturb');
+  console.log('Ready!');
+  });
+
+
+client.on("warn", info => console.log(info));
+
+client.on("error", console.error)
+
+//DEFINIING
+client.commands = new discord.Collection()
+client.prefix = PREFIX
+client.queue = new Map();
+client.vote = new Map();
+
+//LETS LOAD ALL FILES
+const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"))
+for (const file of cmdFiles) {
+  const command = require(join(__dirname, "commands", file))
+  client.commands.set(command.name, command)
+} //LOADING DONE
+
+
+//WHEN SOMEONE MESSAGE
+client.on("message", message => {
+   if (message.author.bot) return;
+  if (!message.guild) return;
+  
+  if(message.content.startsWith(PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
+    
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/) //removing prefix from args
+    const command = args.shift().toLowerCase();
+    
+    if(!client.commands.has(command)) {
+      return;
+    } 
+    
+  try  { //TRY TO GET COMMAND AND EXECUTE
+      client.commands.get(command).execute(client, message, args)
+    //COMMAND LOGS
+    console.log(`${message.guild.name}: ${message.author.tag} Used ${client.commands.get(command).name} in #${message.channel.name}`)
+    } catch (err) { //IF IT CATCH ERROR
+      console.log(err)
+      message.reply("I am getting error on using this command")
+    }
+    
+  }
+  
+  
+});
+
+
+
+
+//DONT DO ANYTHING WITH THIS TOKEN lol
+client.login(process.env.token);
